9163b0f76081ba633038caf758bb6eb5b414d43d
smnde <des.mn69@gmail.com>
2023-01-14 16:02:43 +0700

    protect routes with middleware

--- backend/src/app/routes/api.routes.js
+++ backend/src/app/routes/api.routes.js
@@ -3,6 +3,7 @@ import AuthService from "../services/auth.service.js";
 import UsersService from "../services/users.service.js";
 import CategoriesService from "../services/categories.service.js";
 import ProductsService from "../services/products.service.js";
+import OrdersService from "../services/orders.service.js";
 import { isLogin } from "../middlewares/auth.middleware.js";
 import { isAdmin, isCashier } from "../middlewares/role.middleware.js";
 
@@ -29,5 +30,11 @@ route.get("/products/:id", isLogin, ProductsService.show);
 route.post("/products", isAdmin, ProductsService.store);
 route.put("/products/:id", isAdmin, ProductsService.update);
 route.delete("/products/:id", isAdmin, ProductsService.destroy);
+
+route.get("/orders", isAdmin, OrdersService.index);
+route.get("/orders/:id", isAdmin, OrdersService.show);
+route.post("/orders", isAdmin, isAdmin, OrdersService.store);
+route.put("/orders/:id", isAdmin, OrdersService.update);
+route.delete("/orders/:id", isAdmin, OrdersService.destroy);
 
 export default route;

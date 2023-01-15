cec7e60e84c8c90c63fd43092bdbbea727c769b2
smnde <des.mn69@gmail.com>
2023-01-15 20:43:49 +0700

    get auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "./routes";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useLazyGetAuthUserQuery } from "./services/api/auth.api";
+import { setAuthUser } from "./services/slice/auth.slice";
 
 export default function App() {
+	const dispatch = useDispatch();
+	const { auth } = useSelector((state) => state);
+	const [getAuthUser] = useLazyGetAuthUserQuery();
+
+	useEffect(() => {
+		if (auth.isLogin) {
+			getAuthUser().then(({ data }) => {
+				dispatch(setAuthUser(data));
+			});
+		}
+	}, [auth.isLogin, getAuthUser, dispatch]);
+
 	return <Router />;
 }
